cc0662e1102eee8b84db1589971ba03d15fa112d
nobu <nao114computer@gmail.com>
2023-08-06 09:07:40 +0900

    各コントローラーの作成とルーティングの設定

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only: [:show, :edit]
+  resources :books
   devise_for :users
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
