033a105c0e95c4a707e3382f528cf29f8bced1d5
nobu <nao114computer@gmail.com>
2023-08-10 13:58:09 +0900

    Fix: Typo

--- bookers2/config/webpack/environment.js
+++ bookers2/config/webpack/environment.js
@@ -2,12 +2,12 @@ const { environment } = require('@rails/webpacker')
 
 module.exports = environment
 
-const webpack = require('rebpack')
+const webpack = require('webpack')
 environment.plugins.prepend(
     'Provide',
     new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
-        JQuery: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery',
         Popper: 'popper.js'
     })
 )
