5e0f32d3f468c50a4bb161b580443e3df4ced534
nobu <nao114computer@gmail.com>
2023-08-11 17:28:22 +0900

    Add: ユーザーのShowページ作成とindexページの追加

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    @user = current_user
+    @book = Book.new
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @book = Book.new
+    @books = @user.books
   end
 
   def edit
