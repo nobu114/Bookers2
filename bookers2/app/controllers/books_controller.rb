7c8dee8763ec8bde699d5da1ef9ed4f7b896ebb8
nobu <nao114computer@gmail.com>
2023-08-11 16:20:40 +0900

    Add: 本の投稿機能追加

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -7,7 +7,9 @@ class BooksController < ApplicationController
   
     def create
       @book = Book.new(book_params)
+      @book.user = current_user
       @books = Book.all
+      @user = current_user
       if @book.save
         flash[:notice] = "Book was successfully created."
         redirect_to book_path(@book.id)
