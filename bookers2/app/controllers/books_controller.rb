e07346962506a28e66c3033002d2d60dc480cc99
nobu <nao114computer@gmail.com>
2023-08-12 15:28:13 +0900

    Fix: 不要なthenを削除

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -1,54 +1,56 @@
 class BooksController < ApplicationController
-    def index
-      @book = Book.new
-      @books = Book.all
-      @user = current_user
+  before_action :authenticate_user!
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user = current_user
+    @books = Book.all
+    @user = current_user
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
     end
-  
-    def create
-      @book = Book.new(book_params)
-      @book.user = current_user
-      @books = Book.all
-      @user = current_user
-      if @book.save
-        flash[:notice] = "Book was successfully created."
-        redirect_to book_path(@book.id)
-      else
-        render :index
-      end
-    end
-  
-    def show
-      @new_book = Book.new
-      @book = Book.find(params[:id])
-      @user = @book.user
-    end
-  
-    def edit
-      @book = Book.find(params[:id])
-      if @book.user != current_user then
-        redirect_to books_path
-      end
-    end
-    
-    def update
-      @book = Book.find(params[:id])
-      if @book.update(book_params)
-        flash[:notice] = "Book was successfully updated."
-        redirect_to book_path(@book.id)
-      else
-        render :edit
-      end
-    end
-    
-    def destroy
-      book = Book.find(params[:id])
-      book.destroy
+  end
+
+  def show
+    @new_book = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user != current_user
       redirect_to books_path
     end
-  
-    private
-    def book_params
-      params.require(:book).permit(:title, :body)
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
     end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
